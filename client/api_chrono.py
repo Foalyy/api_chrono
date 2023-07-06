d5d0c09b5077979e270d91adb35dd4633508f486
Anthony <a.gautier@silica.io>
2023-07-06 21:56:26 +0200

    Added a Python client with example

---
+++ client/api_chrono.py
@@ -0,0 +1,99 @@
+import threading, queue, requests
+
+api_url = ""
+api_key = ""
+timeout = 1
+
+main_to_bg_queue = queue.Queue()
+bg_to_main_queue = queue.Queue()
+
+def _bg_thread_worker():
+    while True:
+        message, payload = main_to_bg_queue.get()
+        if message == 'get':
+            resp = requests.get(f'{api_url}/state', timeout=timeout).json()
+            bg_to_main_queue.put(resp)
+        elif message == 'post':
+            requests.post(f'{api_url}/state', headers={'api_key': api_key}, json=payload, timeout=timeout)
+        elif message == 'reset':
+            requests.post(f'{api_url}/reset', headers={'api_key': api_key}, timeout=timeout)
+        main_to_bg_queue.task_done()
+
+bg_thread = threading.Thread(target=_bg_thread_worker, daemon=True)
+bg_thread.start()
+
+def set_api_url(url):
+    global api_url
+    api_url = url
+
+def set_api_key(key):
+    global api_key
+    api_key = key
+
+def post(state):
+    main_to_bg_queue.put(('post', state))
+
+def request_state():
+    main_to_bg_queue.put(('get', None))
+
+def state_response():
+    state = None
+    while not bg_to_main_queue.empty():
+        try:
+            state = bg_to_main_queue.get(block=False)
+        except queue.Empty:
+            break
+    return state
+
+def reset_state():
+    main_to_bg_queue.put(('reset', None))
+
+
+def set_zone_mf(color):
+    post({
+        'zones': {
+            'mf': color
+        }
+    })
+
+def set_zone_fx(color):
+    post({
+        'zones': {
+            'fx': color
+        }
+    })
+
+def set_next_launch(next_launch):
+    post({
+        'next_launch': next_launch
+    })
+
+def clear_next_launch():
+    post({
+        'next_launch': None
+    })
+
+def set_projects_in_clubs_tent(projects):
+    post({
+        'clubs_tent': projects
+    })
+
+def set_projects_in_jupiter(projects):
+    post({
+        'jupiter': projects
+    })
+
+def clear_launchpad(zone, launchpad):
+    post({
+        f'launchpads_{zone.lower()}': {
+            launchpad: None
+        }
+    })
+
+def set_project_on_launchpad(zone, launchpad, project):
+    post({
+        f'launchpads_{zone.lower()}': {
+            launchpad: project
+        }
+    })
+    
---
+++ client/example.py
@@ -0,0 +1,174 @@
+import api_chrono
+
+# Configure the API
+api_chrono.set_api_url("https://chrono.silica.io")
+api_chrono.set_api_key("AeHi8aopuxa9oNgei8ooShai1quahpae")
+
+# Change the colors of the lights
+api_chrono.set_zone_mf('green')
+api_chrono.set_zone_fx('red')
+
+# Clear any project listed in Next launch
+api_chrono.clear_next_launch()
+
+# Update the project in Next launch
+api_chrono.set_next_launch(
+    {
+        'id': 660,
+        'code': 'FX24',
+        'name': 'Cobra',
+        'club_name': 'LéoFly',
+        'rocket_color': 'Noir',
+        'parachute_color': 'Rouge',
+        'launchpad_name': 'Toutatis',
+        'countdown': 180,
+        'countdown_paused': False,
+    }
+)
+
+# Update the list of projects in the clubs tent
+api_chrono.set_projects_in_clubs_tent(
+    [
+        {
+            'arrival_time': 1688649923,
+            'id': 579,
+            'code': 'FX43',
+            'name': 'Cyclone',
+            'club_name': 'AeroIpsa',
+            'rocket_color': 'Vert',
+            'parachute_color': 'Rouge/blanc',
+        },
+        {
+            'arrival_time': 1688649580,
+            'id': 680,
+            'code': 'MF13',
+            'name': 'DART',
+            'club_name': 'Planète Sciences',
+            'rocket_color': 'Gris',
+            'parachute_color': 'Rouge',
+        }
+    ]
+)
+
+# Same parameter as set_projects_in_clubs_tent
+# api_chrono.set_projects_in_jupiter([ ... ])
+
+# Clear the project on a launchpad
+api_chrono.clear_launchpad('fx', 'toutatis')
+
+# Update the project on a launchpad
+api_chrono.set_project_on_launchpad('fx', 'toutatis',
+    {
+        'countdown': 300,
+        'countdown_paused': True,
+        'waiting_time': 180,
+        'arrival_time': 1688648584,
+        'id': 660,
+        'code': 'FX24',
+        'name': 'Cobra',
+        'club_name': 'LéoFly',
+        'rocket_color': 'Noir',
+        'parachute_color': 'Rouge',
+    }
+)
+
+# Update some fields independently
+api_chrono.set_project_on_launchpad('fx', 'toutatis', {
+    'rf': True,
+    'rf_frequency': '868MHz',
+})
+api_chrono.set_project_on_launchpad('fx', 'toutatis', { 'ddo_ok': True })
+api_chrono.set_project_on_launchpad('fx', 'toutatis', {
+    'landed': True,
+    'flight_result': 'nominal',
+})
+
+# Read the current state
+# First we send a non-blocking request for the current state,
+# then we check state_response() until it returns a non-None value
+api_chrono.request_state()
+while True:
+    state = api_chrono.state_response()
+    if state is not None:
+        print(state)
+        break
+    else:
+        # Do something else
+        pass
+
+
+# Update the full state
+full_state = {
+    'zones': {
+        'mf': 'red',
+        'fx': 'green',
+    },
+    'next_launch': {
+        'id': 660,
+        'code': 'FX24',
+        'name': 'Cobra',
+        'club_name': 'LéoFly',
+        'rocket_color': 'Noir',
+        'parachute_color': 'Rouge',
+        'launchpad_name': 'Toutatis',
+        'countdown': 180,
+        'countdown_paused': False,
+    },
+    # 'next_launch': None,
+    'clubs_tent': [
+        {
+            'arrival_time': 1688649923,
+            'id': 579,
+            'code': 'FX43',
+            'name': 'Cyclone',
+            'club_name': 'AeroIpsa',
+            'rocket_color': 'Vert',
+            'parachute_color': 'Rouge/blanc',
+        },
+        {
+            'arrival_time': 1688649580,
+            'id': 680,
+            'code': 'MF13',
+            'name': 'DART',
+            'club_name': 'Planète Sciences',
+            'rocket_color': 'Gris',
+            'parachute_color': 'Rouge',
+        }
+    ],
+    'launchpads_fx': {
+        'toutatis': {
+            'countdown': 300,
+            'countdown_paused': True,
+            'waiting_time': 180,
+            'arrival_time': 1688648584,
+            'id': 660,
+            'code': 'FX24',
+            'name': 'Cobra',
+            'club_name': 'LéoFly',
+            'rocket_color': 'Noir',
+            'parachute_color': 'Rouge',
+            'special_project': False,
+            'upper_ailerons': False,
+            'two_stage': False,
+            'roll_control': False,
+            'supersonic': False,
+            'rf': False,
+            'rf_onboard': False,
+            'rf_check': False,
+            'rf_on': False,
+            'rf_frequency': '',
+            'launchpad_set': False,
+            'motor_type': '',
+            'motor_destocked': False,
+            'motor_inserted': False,
+            'motor_wired': False,
+            'pyro_ok': False,
+            'club_ok': False,
+            'ddo_ok': False,
+            'rso_ok': False,
+            'landed': False,
+            'flight_result': 'unknown',
+        }
+    }
+}
+#api_chrono.post(full_state)
